9e444456c76af803ae6ffeeab7d24dfd232edffc
Brice Sanchez <contact@brice-sanchez.com>
2012-10-12 18:49:58 -0400

    Improve indentation pages_helper.rb

--- app/helpers/spree/pages_helper.rb
+++ app/helpers/spree/pages_helper.rb
@@ -6,8 +6,8 @@ module Spree::PagesHelper
 
   def render_layout_as_partial(slug)
     page = Spree::Page.by_slug(slug).first
-      if page && page.render_layout_as_partial == true
-        render :partial => page.layout, :locals => {:title => page.title, :body => page.body}
-      end
+    if page && page.render_layout_as_partial == true
+      render :partial => page.layout, :locals => {:title => page.title, :body => page.body}
+    end
   end
 end
