c2bc718ce6de7e18ccfb0b955fbdca3413968580
Brice Sanchez <contact@brice-sanchez.com>
2012-10-12 11:18:14 -0400

    Add ability to render a layout as a partial

--- app/helpers/spree/pages_helper.rb
+++ app/helpers/spree/pages_helper.rb
@@ -3,4 +3,11 @@ module Spree::PagesHelper
     page = Spree::Page.by_slug(slug).first
     raw page.body if page
   end
+
+  def render_layout_as_partial(slug)
+    page = Spree::Page.by_slug(slug).first
+      if page && page.render_layout_as_partial == true
+        render :partial => page.layout, :locals => {:title => page.title, :body => page.body}
+      end
+  end
 end
